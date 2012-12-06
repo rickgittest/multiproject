e1d36294b8f60e9e06f0cb0f0bb8ab86a4c114c9
Rick Horowitz <rickhoro@gmail.com>
2012-12-06 12:10:12 -0500

    OPEN - task 3: Another issue created on github https://github.com/rickgittest/multiproject/issues/issue/3. Set up the structure to implement the feature. Feature not yet complete.

--- ManyFilesTester/src/TestClass.java
+++ ManyFilesTester/src/TestClass.java
@@ -13,8 +13,13 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TestClass tc = new TestClass();
+		tc.processNewFeature();
+	}
 
+	private void processNewFeature() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
